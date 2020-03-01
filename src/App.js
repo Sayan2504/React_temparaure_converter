a87b13a0ecbba2effb143426974c00351ee1b89b
Sayan Sinha <sinhasayan888@gmail.com>
2020-03-01 21:39:43 +0530

    implemented conversion without buttons

--- src/App.js
+++ src/App.js
@@ -1,50 +1,31 @@
 import React, { useState } from 'react';
 import './App.css';
-import Centigrade from './form/conversion'
+import Conversion from './form/conversion'
 
 const App = () =>{
-  const[celcius, setCelcius] = useState();
-  const[farhenheit, setFarhenheit] = useState();
-
-  const Farhenheit = (text) =>{
-    text = parseInt(text)
-    setFarhenheit(
-      (text * 9/5) + 32
-    );
-  };
-
-  const Fcalculation = () =>{
-    return farhenheit;
-  };
-
-  const Celcius = (text) =>{
-    text = parseInt(text)
-    setCelcius(
-      (text -32) * (5/9)
-    );
-  };
-
-  const Ccalculation = () =>{
-    return celcius;
-  };
+  const[celcius, setCelcius] = useState("");
+  const[farhenheit, setFarhenheit] = useState("");
+  
 
   const resetC =() =>{
     setCelcius("");
-  }
+  };
+
 
   const resetF =() =>{
     setFarhenheit("");
-  }
+  };
+
 
   const resetAll =() =>{
     setFarhenheit("");
     setCelcius("");
-  }
+  };
   
   return(
     <div className="form">
       <h2>Centigrade to Farhenheit</h2>
-      <Centigrade Fcalculation={Fcalculation} Farhenheit={Farhenheit} Ccalculation={Ccalculation} Celcius={Celcius} resetF={resetF} resetC={resetC} resetAll={resetAll}/>
+      <Conversion resetC={resetC} resetF={resetF} resetAll={resetAll} />
     </div>
   );
 }
--- src/form/conversion.js
+++ src/form/conversion.js
@@ -1,49 +1,47 @@
 import React, {useState} from 'react'
 
-const Conversion = ({Fcalculation, Farhenheit, Ccalculation, Celcius, resetF, resetC, resetAll}) =>{
+const Conversion = ({resetC, resetF, resetAll}) =>{
     const[textC, setTextC] = useState("");
     const[textF, setTextF] = useState("");
 
-    const changeTextC = (e) =>{
-        setTextC(e.target.value);
-    };
+    const cToF = (e) =>{
+        setTextC(e.target.value)
+        setTextF((textF * 9/5) + 32)   
+    }
 
-    const changeTextF = (e) =>{
-        setTextF(e.target.value);
-    };
+    const fToC = (e) =>{
+        setTextF(e.target.value)
+        setTextC((textC - 32) * (5/9))
+    }
 
     return(
         <div>
-            <input type="text" value={textC} value={Ccalculation(textC)}  placeholder="Enter Centigrade temp" onChange={changeTextC}/>
-            <button onClick={(e)=>{
-                    e.preventDefault();
-                    Farhenheit(textC);
-                }}>Change to Farhenheit</button>
+            <input type="text" value={textC} placeholder="Enter Centigrade temp" onChange={cToF}/>
             <button onClick={(e)=>{
                     e.preventDefault();
                     resetC();
                     }}>Reset</button>
-
-
             <br/>
-            <input type="text" value={textF} value={Fcalculation(textF)} placeholder="Enter Farhenheit temp" onChange={changeTextF}/>
-            <button onClick={(e)=>{
-                    e.preventDefault();
-                    Celcius(textF);
-                }}>Change to Celcius</button>
+
+
+
+            <br/><br/><br/>
+            <input type="text" value={textF} placeholder="Enter Farhenheit temp" onChange={fToC}/>
             <button onClick={(e)=>{
                     e.preventDefault();
                     resetF();
                     }}>Reset</button>
-
-
             <br/>
+
+
+
+            <br/><br/>
             <button onClick={(e)=>{
                     e.preventDefault();
                     resetAll();
                     }}>Reset all</button>
         </div>
-    )
+    );
    
 }
 
